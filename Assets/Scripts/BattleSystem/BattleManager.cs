8d15244ce1bbf9586c0a80fdf68b530ade2d2807
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2024-10-20 17:36:26 +0800

    spawn player

--- Assets/Scripts/BattleSystem/BattleManager.cs
+++ Assets/Scripts/BattleSystem/BattleManager.cs
@@ -2,11 +2,23 @@ using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 
 public class BattleManager : Singleton<BattleManager>
 {
-	public AbilityDatabase AbilityDatabase;
-	public HexGrid hexgrid = new HexGrid() ;
+	[Header("Player-related ref")] public AbilityDatabase AbilityDatabase;
+	[SerializeField] private GameObject playerPrefab;
+	[SerializeField] private Vector2Int playerSpawnCoord;
+
+	public HexGrid hexgrid = new HexGrid();
+
+	[Header("Turn Related Ref")] [SerializeField]
+	private float initTurnDur;
+
+	public bool IsPlayerTurn;
+	public bool IsBattleMode = false;
+	public Action<bool> OnTurnSwitch;
+
 	public void InitBattle()
 	{
 		if (AbilityDatabase == null)
@@ -17,12 +29,17 @@ public class BattleManager : Singleton<BattleManager>
 
 		for (int i = 0; i < 3; i++)
 		{
-			Card testCard = CardFactory.Instance.CreateCardFromList(AbilityDatabase,"1", AbilityDatabase.GetRandomAbilityFromList("1").id);
+			Card testCard = CardFactory.Instance.CreateCardFromList(AbilityDatabase, "1",
+				AbilityDatabase.GetRandomAbilityFromList("1").id);
 			CardsManager.Instance.AddCardToDeck(testCard);
 			CardsManager.Instance.DrawCard();
 		}
-		
+
+		InitPlayer();
 		EnemyManager.Instance.InitEnemies();
+
+		IsBattleMode = true;
+		//StartCoroutine(_TurnBaseCoroutine());
 	}
 
 	private void Start()
@@ -32,11 +49,38 @@ public class BattleManager : Singleton<BattleManager>
 
 	private void Update()
 	{
-		
-		
-		
+
+
+
 	}
 
-	
+	private void InitPlayer()
+	{
+		HexCellComponent cell = hexgrid.GetCell(new Vector3Int(playerSpawnCoord.x, 0, playerSpawnCoord.y));
+		if (cell.CellData.CellType == CellType.Empty)
+		{
+			GameObject newInstance = 
+				Instantiate(playerPrefab, new Vector3(cell.transform.position.x,playerPrefab.transform.position.y,cell.transform.position.z)
+					, quaternion.identity);
+			//newInstance.currentCoord = cell.CellData.Coordinates;
+			cell.CellData.CellType = CellType.Player;
+		}
+		else
+		{
+			Debug.LogError("Not valid cell to spawn!");
+		}
+	}
+
+
+
+	private IEnumerator _TurnBaseCoroutine()
+	{
+		while (IsBattleMode)
+		{
+			yield return new WaitForSeconds(initTurnDur);
+		}
+	}
 }
+
+
 
