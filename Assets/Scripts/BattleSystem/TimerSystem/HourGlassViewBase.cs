a8d6cfea02cee046f6f99dc23c9ef6783d10e09e
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2025-01-28 23:26:31 +0800

    hourglass refactor

---
+++ Assets/Scripts/BattleSystem/TimerSystem/HourGlassViewBase.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public abstract class HourGlassViewBase : MonoBehaviour 
+{
+	protected Sequence currentSequence;
+	[SerializeField] protected RectTransform hourGlass;
+	[SerializeField] protected Image sand;
+
+	protected virtual void Awake()
+	{
+		
+	}
+	protected virtual void OnDestroy()
+	{
+		currentSequence?.Kill();
+	}
+
+	public virtual void CountTime(float duration)
+	{
+		currentSequence?.Kill();
+		ResetState();
+		currentSequence = DOTween.Sequence();
+
+		currentSequence.Join(RotateHourGlass());
+		currentSequence.Join(AnimateSand(duration));
+		AddShakeEffect();
+	}
+
+	protected abstract Tween RotateHourGlass();
+	protected abstract Tween AnimateSand(float duration);
+
+	protected virtual void AddShakeEffect()
+	{
+		currentSequence.AppendCallback(() =>
+		{
+			hourGlass.DOShakeRotation(0.5f, 10f, 10, 90f)
+				.SetEase(Ease.OutQuad);
+		});
+	}
+
+	public virtual void StopAnimation()
+	{
+		currentSequence?.Kill();
+		ResetState();
+	}
+
+	protected abstract void ResetState();
+
+	protected void UpdateSandColor(float progress)
+	{
+		Color newColor = progress >= 0.5f 
+			? Color.Lerp(Color.yellow, Color.green, (progress - 0.5f) * 2f)
+			: Color.Lerp(Color.red, Color.yellow, progress * 2f);
+        
+		sand.color = newColor;
+	}
+}
--- Assets/Scripts/BattleSystem/UI/UIHourGlass.cs
+++ Assets/Scripts/BattleSystem/UI/UIHourGlass.cs
@@ -3,85 +3,27 @@ using UnityEngine.UI;
 using DG.Tweening;
 using Unity.Mathematics;
 
-public class UIHourGlass : MonoBehaviour 
+public class UIHourGlass : HourGlassViewBase
 {
-    [SerializeField] private RectTransform hourGlass;
-    [SerializeField] private Image sand;
-
-    private Sequence currentSequence;
-    
-    private void Start() 
+    protected override Tween RotateHourGlass()
     {
-        //BattleManager.Instance.OnTurnStart.AddListener<float>(CountTime); 
+        return hourGlass.DORotate(new Vector3(0, 0, 180f), 0.5f).SetEase(Ease.OutBack);
     }
 
-    private void OnDestroy()
+    protected override Tween AnimateSand(float duration)
     {
-        currentSequence?.Kill();
-    }
-
-    public void CountTime(float duration) 
-    {
-        // Kill any existing sequence
-        currentSequence?.Kill();
-        
-        // Reset state
-        hourGlass.rotation = Quaternion.identity;
-        sand.rectTransform.anchoredPosition = Vector2.zero;
-        sand.color = Color.green;
-
-        currentSequence = DOTween.Sequence();
-
-        // Rotate hourglass 180 degrees
-        currentSequence.Join(
-            hourGlass.DORotate(new Vector3(this.transform.rotation.x,0,180), 0.5f)
-            .SetEase(Ease.OutBack)
-        );
-
-        // Sand movement animation with fixed direction
-        const float sandDistance = 128f; 
-        
-        currentSequence.Join(
-            sand.rectTransform.DOAnchorPosY(
-                sandDistance, 
-                duration
-            )
+        const float sandDistance = 128f;
+        return sand.rectTransform.DOAnchorPosY(sandDistance, duration)
             .SetEase(Ease.Linear)
-            .OnUpdate(() => {
-                // Calculate progress (1 to 0)
-                float currentPos = sand.rectTransform.anchoredPosition.y;
-                float progress = 1 - Mathf.Abs(currentPos / sandDistance);
-                
-
-                // Update color based on progress
-                Color newColor;
-                if (progress >= 0.5f)
-                {
-                    // Green (1.0) to Yellow (0.5)
-                    float t = (progress - 0.5f) * 2f; // Scale 0.5-1.0 to 0-1
-                    newColor = Color.Lerp(Color.yellow, Color.green, t);
-                }
-                else
-                {
-                    // Yellow (0.5) to Red (0.0)
-                    float t = progress * 2f; // Scale 0-0.5 to 0-1
-                    newColor = Color.Lerp(Color.red, Color.yellow, t);
-                }
-                
-                sand.color = newColor;
-            })
-        );
-
-        //Optional shake at the end
-        currentSequence.AppendCallback(() => {
-            hourGlass.DOShakeRotation(0.5f, 10f, 10, 90f)
-                .SetEase(Ease.OutQuad);
-        });
+            .OnUpdate(() =>
+            {
+                float progress = 1 - Mathf.Abs(sand.rectTransform.anchoredPosition.y / sandDistance);
+                UpdateSandColor(progress);
+            });
     }
 
-    public void StopAnimation()
+    protected override void ResetState()
     {
-        currentSequence?.Kill();
         hourGlass.rotation = Quaternion.identity;
         sand.rectTransform.anchoredPosition = Vector2.zero;
         sand.color = Color.green;
